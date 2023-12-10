3c02efa970a5b07f597389960f19bcd2d0476abb
Vasu Ramanujam <90873585+vasu-ramanujam@users.noreply.github.com>
2023-12-10 02:13:04 -0500

    added fxns in preprocess

--- preprocess.py
+++ preprocess.py
@@ -7,6 +7,21 @@ def sample_func():
     print('sample function!')
 
 
+#preprocessing functions:
+def gen_info(df):
+    print(f'Num. of Instances: {df.shape[0]}')
+    print(f'Sample Instance: {df.iloc[13]}')
+
+    print(f'There are {len(df['related_lang'].unique())}Languages Inherited From: {df['related_lang'].unique()}')
+
+
+def get_related(df):
+    return df.loc[df['reltype'] == 'inherited_from' & df['parent_position'] == 0.0]
+
+
+
+
+
 def get_english(filename):
     df = pd.read_csv(filename)
     df_all_eng = df.loc[df['lang'] == 'English']
