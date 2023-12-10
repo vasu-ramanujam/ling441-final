cb6505c500137118fb0738ec727f3a2fc89ae0fc
Vasu Ramanujam <90873585+vasu-ramanujam@users.noreply.github.com>
2023-12-10 01:59:35 -0500

    update preprocess

--- preprocess.py
+++ preprocess.py
@@ -3,11 +3,15 @@ import numpy as np
 import time
 import math
 
-df = pd.read_csv('etymology.csv')
-
-df_all_eng = df.loc[df['lang'] == 'English']
-print(f'shape of all english db: {df_all_eng.shape}')
-df_all_eng.to_csv('all_english.csv', index=False)  
-
 def sample_func():
     print('sample function!')
+
+
+def get_english(filename):
+    df = pd.read_csv(filename)
+    df_all_eng = df.loc[df['lang'] == 'English']
+    print(f'shape of all english db: {df_all_eng.shape}')
+    df_all_eng.to_csv('all_english.csv', index=False)  
+
+if __name__ == "__main__":
+    get_english('etymology.csv')
